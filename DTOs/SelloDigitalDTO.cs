45b29b92f062dd9f986520a44b75ea5b4f07d8f7
JavierLH <javiercitonandes@gmail.com>
2026-03-14 20:27:31 -0600

    Dtos para sellos clientes

---
+++ DTOs/SelloDigitalDTO.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaAduanero.Shared.DTOs
+{
+    public class SelloDigitalCrearDTO
+    {
+        [Required]
+        public string RFC { get; set; }
+
+        public string RazonSocial { get; set; }
+
+        [Required]
+        public IFormFile ArchivoCer { get; set; } // Representa el archivo físico que sube el usuario
+
+        [Required]
+        public IFormFile ArchivoKey { get; set; }
+
+        [Required]
+        public string PasswordKey { get; set; }
+
+        [Required]
+        public string PasswordVucem { get; set; }
+
+        [Required]
+        public DateTime FechaVencimiento { get; set; }
+    }
+}
