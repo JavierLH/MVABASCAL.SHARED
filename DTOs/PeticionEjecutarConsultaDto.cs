1a8e9674f4687213a9d57bf2ea4b254459140a3a
JavierLH <javiercitonandes@gmail.com>
2026-03-18 16:23:03 -0600

    SHARED: DTOs para enviar parametros de digitalizacion

---
+++ DTOs/PeticionEjecutarConsultaDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAduanero.Shared.DTOs
+{
+    public class PeticionEjecutarConsultaDto
+    {
+        // Credenciales WCF
+        public string UsuarioWcf { get; set; }
+        public string PasswordWcf { get; set; }
+
+        // Datos para consultar el Acuse
+        public string RfcSolicitante { get; set; }
+        public long NumeroOperacion { get; set; }
+
+        // Credenciales y archivos de la e.firma (Sellos)
+        public string PasswordLlave { get; set; }
+        public string CertificadoBase64 { get; set; } // Archivo .cer
+        public string LlavePrivadaBase64 { get; set; } // Archivo .key
+    }
+}
---
+++ DTOs/PeticionEjecutarDigitalizacionDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAduanero.Shared.DTOs
+{
+    public class PeticionEjecutarDigitalizacionDto
+    {
+        // Credenciales WCF para la cabecera de seguridad
+        public string UsuarioWcf { get; set; }
+        public string PasswordWcf { get; set; }
+
+        // Datos de negocio para el trámite
+        public string RfcSolicitante { get; set; }
+        public string Correo { get; set; }
+        public int IdTipoDocumento { get; set; }
+        public string NombreDocumento { get; set; } // Sin extensión .pdf
+        public string RfcConsulta { get; set; }
+
+        // Credenciales y archivos de la e.firma (Sellos) y Documento
+        public string PasswordLlave { get; set; }
+        public string CertificadoBase64 { get; set; } // Archivo .cer
+        public string LlavePrivadaBase64 { get; set; } // Archivo .key
+        public string ArchivoPdfBase64 { get; set; } // El documento a digitalizar
+    }
+}
