9ec8204c9479e799636f4a1bfdf6df488e15b46c
JavierLH <javiercitonandes@gmail.com>
2026-05-21 23:17:04 -0600

    [FIX] NUEVO DTO PARA OBTENER CORREOS DE LA TABLA MAESTRA CLIENTES

---
+++ DTOs/CorreoPerfilDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAduanero.Shared.DTOs
+{
+    // DTO para enviar la información al frontend
+    public class CorreoPerfilDto
+    {
+        public int Id { get; set; } // Opcional, dependiendo de si actualizas por ID o haces borrado/inserción
+        public string Email { get; set; } = string.Empty;
+    }
+
+    // DTO para recibir la actualización desde el frontend
+    public class ActualizarCorreosPerfilDto
+    {
+        public List<CorreoPerfilDto> Correos { get; set; } = new List<CorreoPerfilDto>();
+    }
+}
